a62bec40934ebb79b75690eafbab5c950a90a814
vitorgsevero <vitorgsevero@gmail.com>
2019-04-23 01:38:17 -0300

    Sign Up View, CSRF Protection and User Model

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
+var csrf = require('csurf');
+
+var csrfProtection = csrf();
+router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,12 +12,21 @@ router.get('/', function(req, res, next) {
     var productChunks = [];
     var chunckSize = 3;
 
-    for(var i = 0; i < docs.length; i+= chunckSize){
+    for(var i = 0; i < docs.length; i += chunckSize){
       productChunks.push(docs.slice(i, i + chunckSize));
     }
 
     res.render('shop/index', { title: 'Shopping Cart', products: productChunks });
   });
+});
+
+
+router.get('/user/signup', function(req, res, next){
+  res.render('user/signup', {csrfToken: req.csrfToken()});
+});
+
+router.post('/user/signup', function(req, res, next){
+  res.redirect('/');
 });
 
 module.exports = router;
