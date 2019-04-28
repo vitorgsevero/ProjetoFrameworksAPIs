e163e07495d53e7f88138fb97be223b4a07386bf
vitorgsevero <vitorgsevero@gmail.com>
2019-04-28 15:15:23 -0300

    Adding a Session Store

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
 var validator = require('express-validator');
+var MongoStore = require('connect-mongo')(session);
 
 var indexRouter = require('./routes/index');
 var userRoutes = require('./routes/user');
@@ -29,7 +30,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(validator());
 
 app.use(cookieParser());
-app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
+app.use(session(
+  {secret: 'mysupersecret',
+   resave: false,
+   saveUninitialized: false,
+   store: new MongoStore({mongooseConnection: mongoose.Connection}),
+   cookie: {maxAge: 180 * 60 * 1000} //session duration time
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(flash());
 app.use(passport.initialize());
@@ -37,6 +44,7 @@ app.use(passport.session());
 
 app.use(function(req, res, next){
   res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
   next();
 });
 
