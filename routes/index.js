90935a908488c14c992016fb10d2347cc5ab68ab
vitorgsevero <vitorgsevero@gmail.com>
2019-04-29 00:10:40 -0300

    Stripe Payments View

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ var Cart = require('../models/cart');
 var Product = require('../models/product');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  Product.find(function(err, docs){
+router.get('/', function (req, res, next) {
+  Product.find(function (err, docs) {
     var productChunks = [];
     var chunckSize = 3;
 
-    for(var i = 0; i < docs.length; i += chunckSize){
+    for (var i = 0; i < docs.length; i += chunckSize) {
       productChunks.push(docs.slice(i, i + chunckSize));
     }
 
@@ -18,12 +18,12 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/add-to-cart/:id', function(req, res, next){
+router.get('/add-to-cart/:id', function (req, res, next) {
   var productId = req.params.id;
-  var cart = new Cart(req.session.cart ? req.session.cart : {items: {}});
-  
-  Product.findById(productId, function(err, product){
-    if(err){
+  var cart = new Cart(req.session.cart ? req.session.cart : { items: {} });
+
+  Product.findById(productId, function (err, product) {
+    if (err) {
       return res.redirect('/');
     }
     cart.add(product, product.id);
@@ -34,12 +34,21 @@ router.get('/add-to-cart/:id', function(req, res, next){
 
 });
 
-router.get('/shopping-cart', function(req, res, next){
-  if(!req.session.cart){
-    return res.render('shop/shopping-cart', {products: null});
+router.get('/shopping-cart', function (req, res, next) {
+  if (!req.session.cart) {
+    return res.render('shop/shopping-cart', { products: null });
   }
   var cart = new Cart(req.session.cart);
-  res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
+  res.render('shop/shopping-cart', { products: cart.generateArray(), totalPrice: cart.totalPrice });
 });
+
+router.get('/checkout', function (req, res, next) {
+  if (!req.session.cart) {
+    return res.redirect('/shopping-cart');
+  }
+  var cart = new Cart(req.session.cart);
+  res.render('shop/checkout', {total: cart.totalPrice});
+});
+
 
 module.exports = router;
